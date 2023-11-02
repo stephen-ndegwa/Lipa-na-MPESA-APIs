ed1f241f86f0bb73a36f18295432571a5f9159ac
Stephen Ndegwa <105418748+stephenndegwa@users.noreply.github.com>
2023-11-02 11:24:09 +0300

    Create c2b_url_registration.php
    This section demonstrates how to register Confirmation and Validation URLs for the C2B (Customer-to-Business) payment process using Safaricom's API.

---
+++ C2B URLs Confirmation and Validation /c2b_url_registration.php
@@ -0,0 +1,41 @@
+<?php
+
+// API endpoint URL
+$apiUrl = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl";
+
+// Replace with your actual access token
+$accessToken = "Bearer <Access-Token>";
+
+// API request data
+$requestData = json_encode([
+    "ShortCode" => "",
+    "ResponseType" => "",
+    "ConfirmationURL" => "",
+    "ValidationURL" => ""
+]);
+
+// Initialize cURL session
+$ch = curl_init($apiUrl);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Authorization: " . $accessToken,
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the cURL session
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if (curl_errno($ch)) {
+    echo 'cURL error: ' . curl_error($ch);
+}
+
+// Close the cURL session
+curl_close($ch);
+
+// Handle the API response, e.g., print it
+echo $response;
